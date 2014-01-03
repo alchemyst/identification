d0f646993644d6d49345f34e51a25131e8e35678
badenhh <platpadda@gmail.com>
2014-01-03 13:04:44 +0200

    Fixed directory import error for Windows v2

--- fit_heat.py
+++ fit_heat.py
@@ -200,11 +200,11 @@ def spacedmarks(x, y, Nmarks, data_ratio=None):
 if __name__ == "__main__":
     materialplots = {}
     counter = 1
-    dirname = os.path.expanduser('~/Dropbox/Raw')
+    dirname = os.path.expanduser('~/Dropbox/Raw/')
     for f in sys.argv[1:]:
         print f
-        fullname = dirname + "/" + f
-        os.path.normcase(fullname)
+        newd = os.path.normcase(dirname)
+        fullname = newd + f
         experiment, material, response = loadfile(fullname)
 
         print "Material:"
